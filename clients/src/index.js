abb07d38b360635f17018f6f0df78b222a2ba7c0
cyee20-1724226 <cyee20@uw.edu>
2020-12-13 13:37:40 -0800

    Dashboard update

--- clients/src/index.js
+++ clients/src/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-<<<<<<< HEAD
 import Dashboard from "./Components/Dashboard";
-=======
->>>>>>> 885278ed3809eee2e51d0461738a6abdcceb6242
 
 
 ReactDOM.render(<Dashboard />, document.getElementById('root'));
