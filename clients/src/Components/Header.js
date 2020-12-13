b122d2130f8c35c5f9ea6100506196f85d32ab67
cyee20-1724226 <cyee20@uw.edu>
2020-12-13 15:18:08 -0800

    Remodeld Dashboard

--- clients/src/Components/Header.js
+++ clients/src/Components/Header.js
@@ -5,12 +5,15 @@ class Header extends Component {
   render() {
     return (
       <header>
-        <button type="button" className="headerButton">
-          <img src={require("../img/husky.png")} aria-hidden="true" alt="Logo"/> 
-        </button>
         <nav>
-          <p >Post</p>
-          <p >Profile</p>
+          <div className="headerButton">
+            <h1>RateTheCourse</h1>
+            <div className="navTitle">
+              <img src={require("../img/home.png")} aria-hidden="true" alt="Logo"/>
+              <img src={require("../img/user.png")} aria-hidden="true" alt="Logo"/>
+              <img src={require("../img/edit.png")} aria-hidden="true" alt="Logo"/>   
+            </div>
+          </div>
         </nav>
       </header>
     );
