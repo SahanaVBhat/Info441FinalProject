1d584a7e24e16eaced563c06904574ccf946cbef
Paola Vanegas <pvdt@uw.edu>
2020-12-14 15:26:14 -0800

    attempted patch evaluation

--- clients/src/Components/Evaluations/Card.js
+++ clients/src/Components/Evaluations/Card.js
@@ -1,6 +1,7 @@
 import React from "react";
 import api from '../../Constants/APIEndpoints/APIEndpoints';
 import { withRouter } from "react-router";
+import { Link } from 'react-router-dom'
 
 
 class Card extends React.Component {
@@ -13,7 +14,8 @@ class Card extends React.Component {
 
     this.state = {
       evalID: 0,
-      authToken: null
+      authToken: null,
+      newDescr: ""
     };
   }
 
@@ -36,9 +38,9 @@ class Card extends React.Component {
             <h5>Grading fairness: {this.props.classInfo.gradingTechniques}</h5>
             <h5>Comments: {this.props.classInfo.description}</h5>
           </div>
-          <div className="card-buttons">
-            <button type="button" class="btn btn-primary" id="editEval" onClick={this.handleEdit}>Edit</button>
-            <button type="button" class="btn btn-primary" id="deleteEval" onClick={this.handleRemove}>Remove</button>
+          <div className="card-buttons" >
+            <button type="button" class="btn btn-primary" onClick={this.handleEdit}>Edit</button>
+            <button type="button" class="btn btn-primary" onClick={this.handleRemove}>Remove</button>
           </div>
         </div>
       )
@@ -49,6 +51,14 @@ class Card extends React.Component {
       evalID: this.props.classInfo.id,
       authToken: localStorage.getItem("Authorization") || null
     }, () => {
+      // create text box for user to input new description 
+      // var updateDescr = document.createElement("INPUT");
+      // updateDescr.setAttribute("type", "text");
+      // updateDescr.setAttribute("placeholder", "Type updated description here...")
+      // document.getElementById("buttons-" + this.state.evalID).appendChild(updateDescr);
+      // set description in setState to text box value 
+
+      // trigger patchEval()
       this.patchEval()
     })
   }
@@ -88,6 +98,7 @@ class Card extends React.Component {
       "Authorization": this.state.authToken
       })
     });
+
     console.log(response.status)
   }
 }
