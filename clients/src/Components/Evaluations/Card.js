80b15c552c72b00c0f4a8b8ce2307bed079ffd19
Paola Vanegas <pvdt@uw.edu>
2020-12-14 14:14:24 -0800

    added functions for path and delete, finished initial delete eval func

--- clients/src/Components/Evaluations/Card.js
+++ clients/src/Components/Evaluations/Card.js
@@ -1,31 +1,95 @@
 import React from "react";
+import api from '../../Constants/APIEndpoints/APIEndpoints';
 import { withRouter } from "react-router";
 
 
 class Card extends React.Component {
+  setError = (error) => {
+    this.setState({ error })
+  }
 
-goToCarddetails = (cardId) => {
-    localStorage.setItem("selectedCard", cardId);
-    this.props.history.push(cardId);
-}
+  constructor(props) {
+    super(props);
 
-render() {
-    return ( 
-        <div className="card"onClick = {()=>this.goToCarddetails(this.props.classInfo.id.toString())}>
-        <div className="card-body">
-    <h3 className="card-title">Evaluation #{this.props.value+1}</h3>
-          <h5>Credit Type: {this.props.classInfo.creditType}</h5>
-          <h5>Instructors: {this.props.classInfo.instructors.map((instructor) => instructor.name)}</h5>
-          <h5>Quarter: {this.props.classInfo.quarter}</h5>
-          <h5>Credit Type: {this.props.classInfo.creditType}</h5>
-          <h5>Workload: {this.props.classInfo.workload}</h5>
-          <h5>Grading fairness: {this.props.classInfo.gradingTechniques}</h5>
-          <h5>Comments: {this.props.classInfo.description}</h5>
+    this.state = {
+      evalID: 0,
+      authToken: null
+    };
+  }
+
+  goToCarddetails = (cardId) => {
+      localStorage.setItem("selectedCard", cardId);
+      this.props.history.push(cardId);
+  }
+
+  render() {
+      return ( 
+          //<div className="card" onClick = {()=>this.goToCarddetails(this.props.classInfo.id.toString())}>
+          <div className="card">
+          <div className="card-body">
+            <h3 className="card-title">Evaluation #{this.props.value+1}</h3>
+            <h5>Credit Type: {this.props.classInfo.creditType}</h5>
+            <h5>Instructors: {this.props.classInfo.instructors.map((instructor) => instructor.name)}</h5>
+            <h5>Quarter: {this.props.classInfo.quarter}</h5>
+            <h5>Credit Type: {this.props.classInfo.creditType}</h5>
+            <h5>Workload: {this.props.classInfo.workload}</h5>
+            <h5>Grading fairness: {this.props.classInfo.gradingTechniques}</h5>
+            <h5>Comments: {this.props.classInfo.description}</h5>
+          </div>
+          <div className="card-buttons">
+            <button type="button" class="btn btn-primary" id="editEval" onClick={this.handleEdit}>Edit</button>
+            <button type="button" class="btn btn-primary" id="deleteEval" onClick={this.handleRemove}>Remove</button>
+          </div>
         </div>
-      </div>
-    
-    )
+      )
+  }
+
+  handleEdit = () => {
+    this.setState({
+      evalID: this.props.classInfo.id,
+      authToken: localStorage.getItem("Authorization") || null
+    }, () => {
+      this.patchEval()
+    })
+  }
+
+  handleRemove = () => {
+    this.setState({
+      evalID: this.props.classInfo.id,
+      authToken: localStorage.getItem("Authorization") || null
+    }, () => {
+      this.removeEval()
+    })
+  }
+
+  patchEval = async() => {
+    console.log("in patch eval")
+    if (!this.state.authToken) {
+      return;
+    }
+
+    // const response = await fetch(api.base + api.handlers.evaluations + "/" + this.state.evalID, { method: "PATCH" });
+    // if (response.status >= 300) {
+    //   const error = await response.text();
+    //   this.setError(error);
+    //   return;
+    // }
+  }
+
+  removeEval = async() => {
+    if (!this.state.authToken) {
+      return;
+    }
+
+    const response = await fetch(api.base + api.handlers.evaluations + "/" + this.state.evalID, 
+      { 
+      method: "DELETE", 
+      headers: new Headers({
+      "Authorization": this.state.authToken
+      })
+    });
+    console.log(response.status)
   }
 }
 
-export default withRouter(Card);
+  export default withRouter(Card);
