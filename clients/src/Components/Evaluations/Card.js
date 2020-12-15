4c398f5251f6a6c22f7f129a22c791e631a57b96
Paola Vanegas <pvdt@uw.edu>
2020-12-14 17:07:09 -0800

    completed patch evaluations

--- clients/src/Components/Evaluations/Card.js
+++ clients/src/Components/Evaluations/Card.js
@@ -38,27 +38,33 @@ class Card extends React.Component {
             <h5>Grading fairness: {this.props.classInfo.gradingTechniques}</h5>
             <h5>Comments: {this.props.classInfo.description}</h5>
           </div>
-          <div className="card-buttons" >
-            <button type="button" class="btn btn-primary" onClick={this.handleEdit}>Edit</button>
+          <div className="card-buttons">
+            <button type="button" class="btn btn-primary" onClick={this.unhide}>Edit</button>
             <button type="button" class="btn btn-primary" onClick={this.handleRemove}>Remove</button>
+          </div>
+          <div className="card-update-textbox">
+            <input type="text" placeholder="Description..." id={"input-" + this.props.classInfo.id} ref={input => this.updt = input} hidden></input>
+            <button type="button" class="btn btn-primary" id={"submit-" + this.props.classInfo.id} onClick={this.handleEdit} hidden>Submit</button>
           </div>
         </div>
       )
   }
 
+  unhide = () => {
+    this.setState({
+      evalID: this.props.classInfo.id
+    }, () => {
+      document.getElementById("input-" + this.state.evalID).hidden = false;
+      document.getElementById("submit-" + this.state.evalID).hidden = false;
+    })
+  }
+
   handleEdit = () => {
     this.setState({
       evalID: this.props.classInfo.id,
-      authToken: localStorage.getItem("Authorization") || null
+      authToken: localStorage.getItem("Authorization") || null,
+      newDescr: this.updt.value
     }, () => {
-      // create text box for user to input new description 
-      // var updateDescr = document.createElement("INPUT");
-      // updateDescr.setAttribute("type", "text");
-      // updateDescr.setAttribute("placeholder", "Type updated description here...")
-      // document.getElementById("buttons-" + this.state.evalID).appendChild(updateDescr);
-      // set description in setState to text box value 
-
-      // trigger patchEval()
       this.patchEval()
     })
   }
@@ -78,12 +84,29 @@ class Card extends React.Component {
       return;
     }
 
-    // const response = await fetch(api.base + api.handlers.evaluations + "/" + this.state.evalID, { method: "PATCH" });
-    // if (response.status >= 300) {
-    //   const error = await response.text();
-    //   this.setError(error);
-    //   return;
-    // }
+    // PATCH /v1/evaluations/:evaluationID
+    const response = await fetch(api.base + api.handlers.evaluations + "/" + this.state.evalID, 
+      { 
+        method: "PATCH", 
+        headers: new Headers({
+          "Authorization": this.state.authToken,
+          "Content-type": "application/json"
+        }),
+        body: JSON.stringify({description: this.state.newDescr})
+      });
+
+    // hide submit and text input for edit 
+    document.getElementById("input-" + this.state.evalID).hidden = true;
+    document.getElementById("submit-" + this.state.evalID).hidden = true;
+    
+    // check response status for errors
+    if (response.status >= 300) {
+      const error = await response.text();
+      this.setError(error);
+      alert(error)
+    }
+
+    console.log(response.status)
   }
 
   removeEval = async() => {
@@ -91,15 +114,22 @@ class Card extends React.Component {
       return;
     }
 
+    // DELETE /v1/evaluations/:evaluationID
     const response = await fetch(api.base + api.handlers.evaluations + "/" + this.state.evalID, 
       { 
-      method: "DELETE", 
-      headers: new Headers({
-      "Authorization": this.state.authToken
+        method: "DELETE", 
+        headers: new Headers({
+        "Authorization": this.state.authToken
       })
     });
 
-    console.log(response.status)
+    // check response for errors
+    if (response.status >= 300) {
+      const error = await response.text();
+      this.setError(error);
+      alert(error);
+      return;
+    }
   }
 }
 
